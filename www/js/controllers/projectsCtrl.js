60fb653b9aee8f5f77cd843ab4b47de453cbde33
Julie Kramer <thejuliekramer@gmail.com>
2018-09-25 12:47:01 -0400

    compile

--- www/js/controllers/projectsCtrl.js
+++ www/js/controllers/projectsCtrl.js
@@ -6,19 +6,29 @@
     function ProjectsController(cfaApi) {
         var model = this;
 
-        cfaApi.getProjects(function(data) {
-            model.languageOptions = [
-                {'key': 'css', 'value': 'CSS'},
-                {'key': 'groovy', 'value': 'Groovy'},
-                {'key': 'html', 'value': 'HTML'},
-                {'key': 'java', 'value': 'Java'},
-                {'key': 'javascript', 'value': 'JavaScript'},
-                {'key': 'php', 'value': 'PHP'},
-                {'key': 'python', 'value': 'Python'},
-                {'key': 'ruby', 'value': 'Ruby'}
-            ];
-            model.projects = data;
-        });
+        model.currentPage = 1;
+        model.pageSize = 10;
+
+        model.languageOptions = [
+            {'key': 'css', 'value': 'CSS'},
+            {'key': 'groovy', 'value': 'Groovy'},
+            {'key': 'html', 'value': 'HTML'},
+            {'key': 'java', 'value': 'Java'},
+            {'key': 'javascript', 'value': 'JavaScript'},
+            {'key': 'php', 'value': 'PHP'},
+            {'key': 'python', 'value': 'Python'},
+            {'key': 'ruby', 'value': 'Ruby'}
+        ];
+
+        model.getProjects = function(newPage) {
+            cfaApi.getSearch(model.pageSize, newPage, 'repositories').then(function(res) {
+                console.log('res', res);
+                model.totalCount = res.total_count;
+                model.projects = res.items;
+            });
+        };
+
+        model.getProjects();
     }
 
 }());
